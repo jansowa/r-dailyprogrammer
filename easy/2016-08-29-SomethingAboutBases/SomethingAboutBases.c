37c613b4201420749899ef61050a4eaba8c94579
jansowa <jan.piotr.sowa@gmail.com>
2017-02-11 15:19:19 +0100

    [2016-08-29] Challenge #281 [Easy] Something about bases
    Solution of
    https://www.reddit.com/r/dailyprogrammer/comments/504rdh/20160829_challenge_281_easy_something_about_bases/

---
+++ easy/2016-08-29-SomethingAboutBases/SomethingAboutBases.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+int charToDecimal(char number){
+    int result;
+    int intSign = (int) number;
+    if(intSign>47 && intSign<58){
+        return intSign-48;
+    }
+    else{
+        return intSign-87;
+    }
+}
+
+int smallestBase(char number[]){
+    int length = strlen(number);
+    int result = 1;
+    int i=0;
+    while(i<length){
+        int decimalDigit=charToDecimal(number[i]);
+        if(decimalDigit>=result){
+            result=decimalDigit+1;
+        }
+        i++;
+    }
+    return result;
+}
+
+int stringToDec(char number[], int base){
+    int length=strlen(number);
+    int result=0;
+    int i=length-1;
+    int multiplier=1;
+    while(i>=0){
+        result=result+charToDecimal(number[i])*multiplier;
+        multiplier=multiplier*base;
+        i--;
+    }
+    return result;
+}
+
+int main(){
+    char number[100];
+    while(scanf("%s", number)!=EOF){
+        int base = smallestBase(number);
+        printf("base %d => %d\n", base, stringToDec(number, base));
+    }
+    return 0;
+}
